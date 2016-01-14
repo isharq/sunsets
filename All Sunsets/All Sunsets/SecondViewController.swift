5b2f8fc7abdd2f4c11fb5a752a657bacfe391ad4
Haje Jan Kamps <hajejan@kamps.org>
2016-01-14 10:36:18 +0000

    Initial Project Setup

---
+++ All Sunsets/All Sunsets/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  All Sunsets
+//
+//  Created by Haje Jan kamps on 14/01/2016.
+//  Copyright © 2016 Kamps Consulting. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

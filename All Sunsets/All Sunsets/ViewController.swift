deb4c616fd9660a953ca7a5a1821a7d50cc28c17
Haje Jan Kamps <hajejan@kamps.org>
2016-01-19 12:08:47 +0000

    Get Core Location working

--- All Sunsets/All Sunsets/ViewController.swift
+++ All Sunsets/All Sunsets/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var longLabel: UILabel!
+    @IBOutlet weak var latLabel: UILabel!
 
+    // initiate core location
+    private var locationManager = CLLocationManager()
+    
+    // Run on load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initiating Location Services
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        
+        print("Ok, finding location")
+        
+        var currentLocation = locationManager.location?.coordinate
+        
+        latLabel.text = "Latitude: \(currentLocation!.latitude)"
+        longLabel.text = "Longitude: \(currentLocation!.longitude)"
+        
+        
+        
+        
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func getFixPressed(sender: AnyObject) {
+        locationManager.requestLocation()
+    }
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+    }
+    
+    
 
 }
 

5adf0ca4686d4ccf48558b4370c61240cde9c71d
Haje Jan Kamps <hajejan@kamps.org>
2016-01-21 09:29:10 +0000

    Working again. Jesus.

--- All Sunsets/All Sunsets/ViewController.swift
+++ All Sunsets/All Sunsets/ViewController.swift
@@ -10,51 +10,51 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
+    struct globalVars {
+        static var LocationPermissions = true
+        static let locationManager = CLLocationManager()
+    }
     
-    
-    //    @IBOutlet weak var testlabel: UILabel!
     @IBOutlet weak var longLabel: UILabel!
     @IBOutlet weak var latLabel: UILabel!
     @IBOutlet weak var timeNowLabel: UILabel!
     @IBOutlet weak var timeSunsetLabel: UILabel!
     @IBOutlet weak var countdownSunsetLabel: UILabel!
-
-    // initiate core location
     
     func updateCoordinates()
     {
-        let locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
-        
-        let currentLocation = locationManager.location?.coordinate
-        
+        var lat : Double = 1
+        var lon : Double = 1
+
         let defaults = NSUserDefaults.standardUserDefaults()
         
-        var lat : Double = 10.0
-        var lon : Double = 10.0
         
+            globalVars.locationManager.requestWhenInUseAuthorization()
+            globalVars.locationManager.delegate = self
+            globalVars.locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            
+            if  globalVars.locationManager.location?.coordinate == nil
+            {
+                print("No location fix, setting location to 0 as a hack")
+                lat = Double(0)
+                lon = Double(0)
+            }
+            else
+            {
+                let currentLocation =  globalVars.locationManager.location?.coordinate
+                lat = Double(currentLocation!.latitude)
+                lon = Double(currentLocation!.longitude)
+            }
+            
+            defaults.setDouble(lat, forKey: "Lat")
+            defaults.setDouble(lon, forKey: "Lon")
+            // Show what was written
+            print("Updated Lat: \(lat) Lon: \(lon)")
+            
+            // Read back and show
+            (lat,lon) = getCoordinates()
+            print("Readback: Lat: \(lat) Long: \(lon) ")
         
-        if (currentLocation!.latitude > 0)
-        {
-            lat = Double(currentLocation!.latitude)
-            lon = Double(currentLocation!.longitude)
-        }
-        else
-        {
-            lat = Double(0)
-            lon = Double(0)
-        }
-        
-        defaults.setDouble(lat, forKey: "Lat")
-        defaults.setDouble(lon, forKey: "Lon")
-        
-        print("Updated Lat: \(currentLocation!.latitude) Lon: \(currentLocation!.longitude)")
-        
-        (lat,lon) = getCoordinates()
-        
-        print("Readback: Lat: \(lat) Long: \(lon) ")
     }
     
     func destroyCoordinates()
@@ -69,27 +69,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
         let defaults = NSUserDefaults.standardUserDefaults()
         
-        if let latitude : Double = defaults.doubleForKey("Lat")
+        if defaults.objectForKey("Lat") != nil
         {
-            if latitude != 0.0
-            {
-                let lat = defaults.doubleForKey("Lat")
-                let lon = defaults.doubleForKey("Lon")
-                
-                latLabel.text = "Lat: \(lat)"
-                longLabel.text = "Long: \(lon)"
-                
-                return(lat,lon)
-            }
-            else
-            {
-                print("No data found, let's try to recreate it...")
-            }
+            let lat = defaults.doubleForKey("Lat")
+            let lon = defaults.doubleForKey("Lon")
+            
+            latLabel.text = "Lat: \(lat)"
+            longLabel.text = "Long: \(lon)"
+            return(lat,lon)
+        }
+        else
+        {
+            print("No data found, let's try to recreate it...")
+            updateCoordinates()
         }
         
-        // No latitude found? Let's re-generate it:
-        
-        updateCoordinates()
+        // And now let's grab it from the storage again and return it
         let lat = defaults.doubleForKey("Lat")
         let lon = defaults.doubleForKey("Lon")
         
@@ -117,13 +112,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         dateFormatter.timeStyle = .MediumStyle
         
         let (lat,long) = getCoordinates()
-        
         let sunsetTime = GetSunset(lat, longitude: long)
-        
         let timeUntil = NSDate().timeIntervalSinceDate(sunsetTime)
-        
         let secondsToGo : Int = Int(timeUntil*(-1))
-        
         let (h,m,s) = secToTime(secondsToGo)
         
         var timeString = String(format: "%02d", h) + ":"
@@ -135,8 +126,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func timerUpdate()
     {
-        updateTime()
-        updateCountdown()
+            updateTime()
+            updateCountdown()
     }
     
     func updateSunsetLabel(time: NSDate)
@@ -161,9 +152,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let sunsetTime = GetSunset(lat, longitude: lon)
         updateSunsetLabel(sunsetTime)
 
-        var updateTimer: NSTimer!
-        updateTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "timerUpdate", userInfo: nil, repeats: true)
-    
+        // Run a timer to keep the clocks up to date.
+            let updateTimer: NSTimer!
+            updateTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "timerUpdate", userInfo: nil, repeats: true)
+        
+        
+        // Get notification permissions first time
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -189,6 +187,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         return UIStatusBarStyle.LightContent
         
     }
+    
+    
+    func scheduleLocal(sender: AnyObject) {
+        let settings = UIApplication.sharedApplication().currentUserNotificationSettings()
+        
+        if settings!.types == .None {
+            let ac = UIAlertController(title: "Can't schedule", message: "Either we don't have permission to schedule notifications, or we haven't asked yet.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+            return
+        }
+        
+        let notification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: 5)
+        notification.alertBody = "Hey you! Yeah you! Swipe to unlock!"
+        notification.alertAction = "be awesome!"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.userInfo = ["CustomField1": "w00t"]
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        }
     
 }
 
